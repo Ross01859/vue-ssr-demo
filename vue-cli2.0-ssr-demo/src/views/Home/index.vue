55276876117abb65eca5d711a2532e3b70e3626e
wers-apple <wers-apple@wers.local>
2019-04-22 23:40:40 +0800

    vue-cli2.0 初始化

---
+++ vue-cli2.0-ssr-demo/src/views/Home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{mes}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mes: "home"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
